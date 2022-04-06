fd84c0ed990e961ba93171642bd78c9a24a30f38
BennyCao <caobenny122@gmail.com>
2022-04-06 12:17:09 -0700

    needed to make a new repo, so i added all the old stuff back, but i made functions that open text area and a delete textarea function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+openText = document.querySelector('.icons')
+
 const notes = [
   { 
     title: "first note", 
@@ -5,3 +7,69 @@ const notes = [
     id: 1 
   }
 ]
+function removeText(template){
+  const revert = document.querySelector('.write-note-area')
+  revert.innerHTML = ''
+  revert.insertAdjacentHTML('afterbegin', template)
+}
+
+function blank(y){
+  const template = ``
+  return template
+}
+
+function delNote(){
+  const empty = getNote()
+  const newTemplate = blank(empty)
+  removeText(newTemplate)
+}
+
+function newNote(noteBody){
+  const template = `
+    <textarea id = 'noteArea' rows="40" cols="80">Note</textarea>
+    <button class="Save">Save</button>
+    <button class="Delete">Delete</button>
+  `
+  return template
+}
+
+function addtoNav(){
+  const whatever = document.querySelector(".side-note-menu")
+  whatever.innerHTML = ''
+  whatever.insertAdjacentHTML('afterbegin', getTitle)
+}
+
+function displayNote(note) {
+  const noteDisplayArea = document.querySelector('.write-note-area')
+  noteDisplayArea.innerHTML = ''
+  noteDisplayArea.insertAdjacentHTML('afterbegin', note)
+}
+function getNote (){
+  const textArea = document.querySelector('.icons')
+  const note = textArea.value
+  return note
+}
+
+function assembleNote (){
+  const noteText = getNote()
+  const note = newNote(noteText)
+  displayNote(note)
+  savBtn = document.querySelector('.Save')
+  delBtn = document.querySelector('.Delete')
+  savBtn.addEventListener('click', )
+  delBtn.addEventListener('click', delNote)
+}
+
+
+function checkNote(){
+  const text = document.getElementById("noteArea")
+  const noteBody = text.value.split("\n")
+  return noteBody
+}
+
+function getTitle(){
+  const savedNote = checkNote()
+  const title = savedNote[0]
+}
+
+openText.addEventListener('click', assembleNote)
